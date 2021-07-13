faf46b3ddac68aa330406e0c462e8108d79f6a7b
paraschevanegru <parascheva.negru@gmail.com>
2021-07-13 23:23:08 +0300

    User functionality

---
+++ ConsentCollector/ConsentCollector.Business/Consent/Services/Users/IUserService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsentCollector.Business.Consent.Models;
+using ConsentCollector.Business.Consent.Models.Users;
+
+namespace ConsentCollector.Business.Consent.Services.Users
+{
+    public interface IUserService
+    {
+        IEnumerable<UserModel> GetAll();
+        Task<UserModel> GetById(Guid id);
+
+        Task<UserModel> Create(CreateUserModel model);
+
+        Task Delete(Guid userId);
+
+        Task Update(Guid userId, CreateUserModel model);
+    }
+}
+ 
---
+++ ConsentCollector/ConsentCollector.Business/Consent/Services/Users/UserService.cs
@@ -0,0 +1,68 @@
+﻿using ConsentCollector.Business.Consent.Models.Users;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AutoMapper;
+using ConsentCollector.Entities.Consent;
+using ConsentCollector.Persistence.UserRepository;
+
+namespace ConsentCollector.Business.Consent.Services.Users
+{
+    public sealed class UserService : IUserService
+    {
+        private readonly IUserRepository userRepository;
+        private readonly IMapper mapper;
+
+        public UserService(IUserRepository userRepository, IMapper mapper)
+        {
+            this.userRepository = userRepository;
+            this.mapper = mapper;
+        }
+        public async Task<UserModel> Create(CreateUserModel model)
+        {
+            var user = this.mapper.Map<User>(model);
+
+            await this.userRepository.Create(user);
+
+            await this.userRepository.SaveChanges();
+
+            return mapper.Map<UserModel>(user);
+        }
+
+        public async Task Delete(Guid userId)
+        {
+            var user = await userRepository.GetUserById(userId);
+
+            userRepository.Delete(user);
+
+            await userRepository.SaveChanges();
+        }
+
+        public IEnumerable<UserModel> GetAll()
+        {
+            var users = userRepository.GetAll();
+
+            return mapper.Map<IEnumerable<UserModel>>(users);
+        }
+
+        public async Task<UserModel> GetById(Guid id)
+        {
+            var user = await userRepository.GetUserById(id);
+
+            return mapper.Map<UserModel>(user);
+        }
+
+        public async Task Update(Guid userId, CreateUserModel model)
+        {
+            var user = await userRepository.GetUserById(userId);
+
+            mapper.Map(model, user);
+
+            userRepository.Update(user);
+
+            await userRepository.SaveChanges();
+        }
+    }
+}
